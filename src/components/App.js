449bbdaea382cdb3887cf416ff52a0e030fe6671
Folklore Labs <git@folklorelabs.io>
2023-01-20 18:38:42 -0800

    better illustration of calculations

--- src/components/App.js
+++ src/components/App.js
@@ -70,15 +70,60 @@ function App() {
   const btnAttackerRemoveHandler = useCallback((uuid) => {
     zkillPointsDispatch(unloadInvolved(uuid));
   }, [zkillPointsDispatch]);
+  const averageAttackerSize = useMemo(() => {
+    const attackerShips = zkillPointsState.involvedShips;
+    return Math.max(1, attackerShips.reduce((total, ship) => {
+      return total + (ship.name === 'Capsule' ? Math.pow(5, zkillPointsState.victimShip.rigSize + 1) : Math.pow(5, ship.rigSize));
+    }, 0) / attackerShips.length);
+  }, [zkillPointsState]);
   
   return (
-    <Box sx={{ width: '100%', maxWidth: 500 }}>
-      <Typography variant="h4" gutterBottom>
+    <Box sx={{ width: '100%', maxWidth: 500, margin: '0 auto' }}>
+      <Typography variant="h4" gutterBottom sx={{ textAlign: 'center' }}>
         Zkill Point Appraisal
       </Typography>
+      <div style={{ textAlign: 'center' }}>
+        <FormControl sx={{ m: 1, minWidth: 320 }} size="small">
+          <TextField
+            id="eft-input"
+            label="EFT"
+            multiline
+            maxRows={15}
+            variant="standard"
+            inputRef={txtEftRef}
+            />
+        </FormControl>
+        <div>
+          <Button
+            onClick={btnEftHandler}
+            >Submit</Button>
+        </div>
+        <FormControl sx={{ m: 1, minWidth: 320 }} size="small">
+          <Autocomplete
+            id="attacker-select"
+            options={SHIPS.sort((a, b) => `${a.rigSize} ${a.group}`.localeCompare(`${b.rigSize} ${b.group}`))}
+            groupBy={(option) => `${option.group} (${Math.pow(5, option.rigSize)} points)`}
+            getOptionLabel={(option) => option.name}
+            sx={{ width: 300 }}
+            renderInput={(params) => <TextField {...params} inputRef={selInvolvedRef} label="Ship" variant="standard" />}
+            // onChange={(event, newValue) => {
+              //   if (!newValue || !newValue.name) return;
+              //   btnAttackerAddHandler(newValue.name);
+              //   event.target.value = '';
+              // }}
+              clearOnEscape
+              />
+        </FormControl>
+        <div>
+          <Button
+            onClick={btnAttackerAddHandler}
+            >Add attacker</Button>
+        </div>
+      </div>
       {state.victimShip ? (
         <>
-          <Typography variant="h2" gutterBottom>
+          <Divider sx={{ margin: '3em 0' }} />
+          <Typography variant="h2" gutterBottom sx={{ textAlign: 'center' }}>
             {state.totalPoints} Points
           </Typography>
           <Typography variant="h5" gutterBottom>
@@ -86,25 +131,31 @@ function App() {
           </Typography>
           <ul>
             <li>
-              <Tooltip title="Determined by rig slot size">
+              <Tooltip title={`Determined by rig slot size (5 ^ ${state.victimShip.rigSize})`}>
                 <span>{state.victimShip.name} ({state.victimBasePoints} points)</span>
               </Tooltip>
             </li>
             <li>
-              <Tooltip title="Based on meta value of DDAs and modules with heat. Mining harvesters reduce by same amount.">
+              <Tooltip title={`The sum of all fitted "dangerous" modules flagged as a High Slot, Mid Slot, Low Slot, or SubSystem at time of death. Modules are considered "dangerous" if they are a Drone Damage mod or if they can be overheated. Likewise, Mining Lasers actually reduce this value.`}>
                 <span>Danger Factor ({state.victimDangerFactor} points)</span>
               </Tooltip>
               <ul>
-                {state.victimShip.modules.filter(module => module.dangerFactor !== 0).map((module, i) => (
+                {state.victimShip.modules.map((module, i) => (
                   <li
                     key={module.uuid}
                     value={module.id}
+                    style={{ opacity: module.dangerFactor === 0 ? 0.5 : 1 }}
                   >
-                    <Tooltip title={`This module ${module.hasHeat ? 'has heat' : ''}${module.isDroneMod ? 'is a drone dmg mod' : ''}${module.isMiningMod ? 'is a mining harvester' : ''}`}>
+                    <Tooltip title={`This module ${module.dangerFactor === 0 ? 'is not factored into point tally.' : ''} ${module.hasHeat ? 'is factored into point tally because it has heat damage.' : ''}${module.isDroneMod ? 'is factored into point tally because it is a drone damage mod.' : ''}${module.isMiningMod ? 'is factored into point tally because it is a mining harvester.' : ''}${module.hasHeat || module.isDroneMod || module.isMiningMod ? ` The point value is based on the module's meta level (${module.metaLevel}). Calculation is 1 + floor(metaLevel / 2)).` : ''}`}>
                       <span>{module.name} ({module.dangerFactor} points)</span>
                     </Tooltip>
                   </li>
                 ))}
+                <li style={{ opacity: 0.5 }}>
+                  <Tooltip title="Only modules equiped on the ship flagged as High Slot, Mid Slot, Low Slot, or SubSystem are considered for point tally. Cargo items ignored.">
+                    <span>Cargo Items (0 points)</span>
+                  </Tooltip>
+                </li>
               </ul>
             </li>
           </ul>
@@ -113,73 +164,49 @@ function App() {
           </Typography>
           <ul>
             <li>
-              <span>Involved Ships ({zkillPointsState.involvedShips.length})</span>
+              <Tooltip title="Reduces points exponentially based on the number of attackers.">
+                <span>Blob Multiplier Penalty: {1 / state.involvedQtyPenalty}</span>
+              </Tooltip>
+            </li>
+            <li>
+              <Tooltip title="Apply a bonus/penalty from -50% to 20% depending on average size of attacking ships. For example: Smaller ships blowing up bigger ships get a bonus or bigger ships blowing up smaller ships get a penalty.">
+                <span>Ship Size Multiplier: {state.involvedSizeMultiplier}</span>
+              </Tooltip>
               <ul>
-                {zkillPointsState.involvedShips.map((ship) => (
-                  <li
-                    key={ship.uuid}
-                  >
-                    {ship.name} ({Math.pow(5, ship.rigSize)} points)
-                    <IconButton
-                      aria-label="delete"
-                      size="small"
-                      onClick={() => btnAttackerRemoveHandler(ship.uuid)}
-                    >
-                      <DeleteIcon fontSize="inherit" />
-                    </IconButton>
-                  </li>
-                ))}
+                <li>
+                <Tooltip title="Average ship size of all attacking ships. Based on rig size for direct comparison to victim ship.">
+                  <span>Average: {averageAttackerSize}</span>
+                </Tooltip>
+                  <ul>
+                    {zkillPointsState.involvedShips.map((ship) => (
+                      <li
+                        key={ship.uuid}
+                      >
+                        <Tooltip title={ship.name === 'Capsule' ? 'Capsules are equal to victim ship rig size + 1.' : `Determined by rig slot size (5 ^ ${ship.rigSize})`}>
+                          <span>{ship.name} ({ship.name === 'Capsule' ? Math.pow(5, state.victimShip.rigSize + 1) : Math.pow(5, ship.rigSize)} points)</span>
+                        </Tooltip>
+                        <IconButton
+                          aria-label="delete"
+                          size="small"
+                          onClick={() => btnAttackerRemoveHandler(ship.uuid)}
+                        >
+                          <DeleteIcon fontSize="inherit" />
+                        </IconButton>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
               </ul>
             </li>
-            <li>
-              <Tooltip title="Reduces points exponentially based on the number of attackers.">
-                <span>Involved Qty Penalty: {state.involvedQtyPenalty}</span>
-              </Tooltip>
-            </li>
-            <li>
-              <Tooltip title="Apply a bonus/penalty from -50% to 20% depending on average size of attacking ships. For example: Smaller ships blowing up bigger ships get a bonus or bigger ships blowing up smaller ships get a penalty. Also uses rig slot size for comparison and averages all attacking ships involved. Capsules are equal to victim ship rig size + 1.">
-                <span>Involved Size Multiplier: {state.involvedSizeMultiplier}</span>
-              </Tooltip>
-            </li>
           </ul>
-          <a href={url}>{url.length < 60 ? url : `${url.slice(0, 40)}...${url.slice(url.length - 20, url.length)}`}</a>
+          <Typography variant="h5" gutterBottom>
+            Static URL
+          </Typography>
+          <p>
+            <a href={url}>{url.length < 60 ? url : `${url.slice(0, 40)}...${url.slice(url.length - 20, url.length)}`}</a>
+          </p>
         </>
       ) : ''}
-      <Divider />
-      <FormControl sx={{ m: 1, minWidth: 320 }} size="small">
-        <TextField
-          id="eft-input"
-          label="EFT"
-          multiline
-          maxRows={15}
-          variant="standard"
-          inputRef={txtEftRef}
-        />
-      </FormControl>
-      <div>
-        <Button
-          onClick={btnEftHandler}
-        >Submit</Button>
-      </div>
-      <Autocomplete
-        id="attacker-select"
-        options={SHIPS.sort((a, b) => `${a.rigSize} ${a.group}`.localeCompare(`${b.rigSize} ${b.group}`))}
-        groupBy={(option) => `${option.group} (${Math.pow(5, option.rigSize)} points)`}
-        getOptionLabel={(option) => option.name}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} inputRef={selInvolvedRef} variant="standard" />}
-        // onChange={(event, newValue) => {
-        //   if (!newValue || !newValue.name) return;
-        //   btnAttackerAddHandler(newValue.name);
-        //   event.target.value = '';
-        // }}
-        clearOnEscape
-      />
-      <div>
-        <Button
-          onClick={btnAttackerAddHandler}
-        >Add attacker</Button>
-      </div>
     </Box>
   );
 }
