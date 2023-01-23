f8e0b8576ea498cee24d18d63d68e91b38822e1f
Folklore Labs <git@folklorelabs.io>
2023-01-23 04:40:41 +0000

    Updates

--- scripts/deploy.js
+++
@@ -1,9 +0,0 @@
-var ghpages = require('gh-pages');
-
-ghpages.publish('build', function(err) {
-    if (err) {
-        console.log('Uh oh.', err);
-    } else {
-        console.log('Deployment done-zo!');
-    }
-});
