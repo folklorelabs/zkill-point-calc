f34b93acf1ce945fa63af3c644df51fa31f80a4f
Folklore Labs <git@folklorelabs.io>
2023-01-24 02:35:31 +0000

    extract get eve dump for later use

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -1,33 +1,14 @@
-const fs = require('fs');
 const path = require('path');
-const csv = require('csvtojson');
 
-const ensureRecursiveSync = require('./utils/ensureRecursiveSync');
-
-const BASE_DUMP_URL = 'https://www.fuzzwork.co.uk/dump/latest';
-
-async function getCsv(url) {
-  const res = await fetch(url);
-  const data = await res.text();
-  return new Promise((resolve) => {
-    csv().fromString(data).then(resolve);
-  });
-}
-
-async function getAndSaveCsv(csvName) {
-  const data = await getCsv(`${BASE_DUMP_URL}/${csvName}.csv`);
-  const dest = path.resolve(__dirname, `../temp/${csvName}.json`);
-  ensureRecursiveSync(path.dirname(dest));
-  await fs.writeFileSync(dest, JSON.stringify(data));
-  return data;
-}
+const getEveDataDump = require('./utils/getEveDataDump');
 
 (async () => {
+  const destDir = path.resolve(__dirname, './temp');
   await Promise.all([
-    getAndSaveCsv('invCategories'),
-    getAndSaveCsv('invTypes'),
-    getAndSaveCsv('invGroups'),
-    getAndSaveCsv('dgmAttributeCategories'),
-    getAndSaveCsv('dgmAttributeTypes'),
+    getEveDataDump('invCategories', destDir),
+    getEveDataDump('invTypes', destDir),
+    getEveDataDump('invGroups', destDir),
+    getEveDataDump('dgmAttributeCategories', destDir),
+    getEveDataDump('dgmAttributeTypes', destDir),
   ]);
 })();
